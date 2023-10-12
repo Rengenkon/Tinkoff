c72bea47c8d32606d097029a8454f78bb9f29d6f
Rengenkon <konnov.rinat.mail.ru@gmail.com>
2023-10-12 20:42:01 +0400

    Done Task 2

--- src/main/java/edu/hw01/Task02.java
+++ src/main/java/edu/hw01/Task02.java
@@ -1,4 +1,17 @@
 package edu.hw01;
 
 public class Task02 {
+    private Task02(){
+    }
+
+    public static int countDigits(long number){
+        int count  = 0;
+
+        do{
+            count++;
+            number /= 10;
+        }while (number != 0);
+
+        return count;
+    }
 }
