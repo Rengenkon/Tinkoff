891576fd348a89a5b7c6e45ff242dd0b2de847f0
Rengenkon <konnov.rinat.mail.ru@gmail.com>
2023-10-22 22:03:46 +0400

    May be working streams

--- src/test/java/edu/project01/ATest.java
+++ src/test/java/edu/project01/ATest.java
@@ -2,13 +2,32 @@ package edu.project01;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ATest {
+    private final InputStream modIn = new InputStream() {
+        @Override
+        public int read() throws IOException {
+            return 0;
+        }
+    };
+    private final PrintStream modOut = new PrintStream(new OutputStream() {
+        @Override
+        public void write(int b) throws IOException {
+
+        }
+    });
+
     @Test
     @DisplayName("")
     void nul (){
         //given
+        Game game = new Game("", modIn, modOut);
         //when
         //then
     }
