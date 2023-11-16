1bb6a27b135b0d9fd2d0332d2dfd9f3948dfa9dd
Rengenkon <konnov.rinat.mail.ru@gmail.com>
2023-11-16 13:35:36 +0400

    to

--- src/main/java/Task01.java
+++ src/main/java/Task01.java
@@ -1,2 +1,37 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task01 {
+    public static long durationMinut(String string) {
+        Matcher matcher = Pattern.compile("(\\d{4}-\\d{2}-\\d{2}, \\d{2}:\\d{2}) - (\\d{4}-\\d{2}-\\d{2}, \\d{2}:\\d{2})").matcher(string);
+        if (matcher.find()) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm");
+            LocalDateTime enter = LocalDateTime.parse(matcher.group(1), formatter);
+            LocalDateTime exit = LocalDateTime.parse(matcher.group(2), formatter);
+            Duration dur = Duration.between(enter, exit);
+            if (dur.toMinutes() > 0) {
+                return dur.toMinutes();
+            }
+        }
+        return 0;
+    }
+
+    public static String avarageTime(String[] strings){
+        final int minInHour = 60;
+        long alltime = 0;
+        for (String string : strings) {
+            alltime += durationMinut(string);
+        }
+        double minut = alltime * 1.0 / strings.length;
+        int hours = 0;
+        while (minut > minInHour) {
+            hours++;
+            minut -= minInHour;
+        }
+        return hours + "ч " + (long)minut + "м";
+    }
+
 }
--- src/test/java/Task01Test.java
+++ src/test/java/Task01Test.java
@@ -1,2 +1,78 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class Task01Test {
+    @Test
+    @DisplayName("Valid")
+    public void f1(){
+        //given
+        String label = "2022-03-12, 20:20 - 2022-03-12, 23:50.";
+        //when
+        long duration = Task01.durationMinut(label);
+        //then
+        assertThat(duration).isEqualTo(210);
+    }
+
+    @Test
+    @DisplayName("Day")
+    public void f2(){
+        //given
+        String label = "2022-03-12, 20:20 - 2022-03-13, 20:20.";
+        //when
+        long duration = Task01.durationMinut(label);
+        //then
+        assertThat(duration).isEqualTo(24 * 60);
+    }
+
+    @Test
+    @DisplayName("Negative value")
+    public void f3(){
+        //given
+        String label = "2022-03-12, 23:50 - 2022-03-12, 20:20.";
+        //when
+        long duration = Task01.durationMinut(label);
+        //then
+        assertThat(duration).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("One label")
+    public void f4(){
+        //given
+        String[] label = new String[]{
+            "2022-03-12, 20:20 - 2022-03-12, 23:50"
+        };
+        //when
+        String avarage = Task01.avarageTime(label);
+        //then
+        assertThat(avarage).isEqualTo("3ч 30м");
+    }
+
+    @Test
+    @DisplayName("Valid values")
+    public void f5(){
+        //given
+        String[] label = new String[]{
+            "2022-03-12, 20:20 - 2022-03-12, 23:50",
+            "2022-03-12, 00:20 - 2022-03-13, 23:50"
+        };
+        //when
+        String avarage = Task01.avarageTime(label);
+        //then
+        assertThat(avarage).isEqualTo("25ч 30м");
+    }
+
+    @Test
+    @DisplayName("Not int min")
+    public void f6(){
+        //given
+        String[] label = new String[]{
+            "2022-03-12, 20:20 - 2022-03-12, 20:21",
+            "2022-03-12, 20:20 - 2022-03-12, 20:22"
+        };
+        //when
+        String avarage = Task01.avarageTime(label);
+        //then
+        assertThat(avarage).isEqualTo("0ч 1м");
+    }
 }
