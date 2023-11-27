dcd311bb50e3022e64bbdb9c601b77f22a4a878d
Rengenkon <konnov.rinat.mail.ru@gmail.com>
2023-11-27 11:59:58 +0400

    4

--- src/main/java/edu/hw07/Task04.java
+++ src/main/java/edu/hw07/Task04.java
@@ -1,4 +1,35 @@
 package edu.hw07;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Task04 {
+    public static double pi(long n) {
+        final int THREAD = 4;
+        final int r = 15_000;
+        final Random random = new Random(System.nanoTime());
+        AtomicLong count = new AtomicLong();
+        ArrayList<Thread> threads = new ArrayList<>(THREAD);
+        for (int i = 0; i < THREAD; i++) {
+            threads.add(new Thread(() -> {
+                for (long j = 0; j < n / THREAD; j++) {
+                    int x = random.nextInt(-r, r + 1);
+                    int y = random.nextInt(-r, r + 1);
+                    if (x * x + y * y < r * r){
+                        count.getAndIncrement();
+                    }
+                }
+            }));
+        }
+        threads.forEach(Thread::start);
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return count.get() * 4.0 / (n - n % THREAD);
+    }
 }
--- src/test/java/edu/hw07/Task04Test.java
+++ src/test/java/edu/hw07/Task04Test.java
@@ -1,4 +1,18 @@
 package edu.hw07;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class Task04Test {
+    @Test
+    @DisplayName("sa")
+    void f() {
+        //given
+        long n = 10_000_000;
+        //when
+        double p = Task04.pi(n);
+        //then
+        assertThat(p).isEqualTo(3.14);
+    }
 }
